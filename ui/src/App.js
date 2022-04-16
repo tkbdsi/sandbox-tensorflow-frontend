fcc0ccb42ee61b43ae59eeaa470e9b6cd837d10f
tkbdsi <patient-remix-0q@icloud.com>
2022-04-16 14:14:57 -0400

    feat: implemented sine function

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+// Third Party Libraries
+import * as tf from '@tensorflow/tfjs';
+
+const makeSineFunction = () => {
+
+  const twoPi = tf.scalar(2.0 * Math.PI);
+  const xdata = tf.mul(twoPi, tf.range(-0.5,0.5,0.01));
+  const ydata = tf.sin(xdata);
+
+  return [Array.from(xdata.dataSync()), Array.from(ydata.dataSync())];
+}
 
 const App = () => {
+
+  const [xdata, ydata] = makeSineFunction()
+
+  console.log(xdata);
+
   return (
     <main>
       <h1>Tensorflow Sandbox</h1>
